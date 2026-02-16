d7c0659573424f52535ca8c8f5d5642c42e34713
Cotidie User <cotidie@example.com>
2026-02-16 20:52:10 -0300

    ugu

--- src/lib/prayers/oraciones/comunion-espiritual-antes.ts
+++ src/lib/prayers/oraciones/comunion-espiritual-antes.ts
@@ -4,7 +4,7 @@ export const comunionEspiritualAntes: Prayer = {
   id: 'comunion-espiritual-antes',
   categoryId: 'oraciones',
   title: 'Oración antes de la comunión',
-  content: `Comunión espiritual
+  content: `*Comunión espiritual*
 
 Yo quisiera, Señor, recibiros,
 con aquella pureza, humildad y devoción,
