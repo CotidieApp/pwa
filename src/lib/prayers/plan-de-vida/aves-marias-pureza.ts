7ac9c1b0632c1d627d72a7e18d8fbf71bc3e9ecf
Cotidie User <cotidie@example.com>
2026-02-16 22:30:26 -0300

    ih

--- src/lib/prayers/plan-de-vida/aves-marias-pureza.ts
+++ src/lib/prayers/plan-de-vida/aves-marias-pureza.ts
@@ -6,18 +6,18 @@ export const avesMariasPureza: Prayer = {
   categoryId: 'plan-de-vida',
   content: `*(De rodillas)*
 
-**Por el poder que te concedió el Padre Eterno.**
+*Por el poder que te concedió el Padre Eterno.*
 
 Dios te salve, María, llena eres de gracia, el Señor es contigo, bendita tú eres entre todas las mujeres y bendito es el fruto de tu vientre, Jesús.
 Santa María, Madre de Dios, ruega por nosotros pecadores, ahora y en la hora de nuestra muerte.
 
-**Por la sabiduría que te concedió El Hijo.**
+*Por la sabiduría que te concedió El Hijo.*
 
 Dios te salve, María, llena eres de gracia, el Señor es contigo, bendita tú eres entre todas las mujeres y bendito es el fruto de tu vientre, Jesús.
 Santa María, Madre de Dios, ruega por nosotros pecadores, ahora y en la hora de nuestra muerte.
 
-**Por el amor que te concedió el Espíritu Santo.**
+*Por el amor que te concedió el Espíritu Santo.*
 
-Dios te salve, María, llena eres de gracia, el Señor es contigo, bendita tú eres entre todas las mujeres y bendito es el fruto de tu vientre, Jesús.
+_Dios_ te salve, María, llena eres de gracia, el Señor es contigo, bendita tú eres entre todas las mujeres y bendito es el fruto de tu vientre, Jesús.
 Santa María, Madre de Dios, ruega por nosotros pecadores, ahora y en la hora de nuestra muerte.`
 };
